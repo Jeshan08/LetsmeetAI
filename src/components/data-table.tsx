f4830724b34aacc869430b43000e950af91deb62
Jeshan08 <jeshanchhabra26@gmail.com>
2026-03-25 12:04:31 -0400

    added meetings data table

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -16,8 +16,8 @@ import {
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
-  data: TData[]
-  onRowClick? :(row:TData) => void 
+  data: TData[],
+  onRowClick?: (row: TData) => void;
 }
 
 export function DataTable<TData, TValue>({
@@ -32,22 +32,19 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="rounded-md border">
+    <div className="rounded-lg border bg-background overflow-hidden">
       <Table>
         <TableBody>
           {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
               <TableRow
-                onClick={()=>{ onRowClick?.(row.original)}}
+                onClick={() => onRowClick?.(row.original)}
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
                 className="cursor-pointer"
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell 
-                  key={cell.id}
-                  className="text-sm p-4"
-                  >
+                  <TableCell key={cell.id} className="text-sm p-4">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
