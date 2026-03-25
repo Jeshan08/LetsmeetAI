877394641620c8dcda4276153d6c660c4525f4b3
Jeshan08 <jeshanchhabra26@gmail.com>
2026-03-25 17:46:42 -0400

    added the meeting variants : different states of meetings

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -10,6 +10,10 @@ import { useTRPC } from "@/trpc/client";
 import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog copy";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
+import { ActiveState } from "../components/active-state";
+import { UpcomingState } from "../components/upcoming-state";
 
 interface Props {
   meetingId: string;
@@ -48,6 +52,12 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     await removeMeeting.mutateAsync({ id: meetingId });
   };
 
+  const isActive = data.status === "active";
+  const isUpcoming = data.status === "upcomming";
+  const isCancelled = data.status === "cancelled";
+  const isCompleted = data.status === "completed";
+  const isProcessing = data.status === "processing";
+
 
   return (
     <>
@@ -64,7 +74,16 @@ export const MeetingIdView = ({ meetingId }: Props) => {
           onEdit={() => setUpdateMeetingDialogOpen(true)}
           onRemove={handleRemoveMeeting}
         />
-        {JSON.stringify(data,null,2)}
+        {isCancelled && <CancelledState />}
+        {isProcessing && <ProcessingState />}
+        {/* {isCompleted && <CompletedState
+         data={data} />} */}
+        {isActive && <ActiveState meetingId={meetingId} />}
+        {isUpcoming && (
+          <UpcomingState
+            meetingId={meetingId}
+          />
+        )}
       </div>
     </>
   )
