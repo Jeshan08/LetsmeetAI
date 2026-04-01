e5487daccf394b1411164d9fbdcee0e7fae7dd8d
Jeshan08 <jeshanchhabra26@gmail.com>
2026-04-01 13:13:38 -0400

    Stability?

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -187,8 +187,13 @@ export async function POST(req: NextRequest) {
       })
       .where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")));
   }else if (eventType === "call.transcription_ready") {
+    
     const event = payload as CallTranscriptionReadyEvent;
+    console.log("FULL TRANSCRIPTION EVENT:", JSON.stringify(event, null, 2)); // ← raw payload
     const meetingId = event.call_cid.split(":")[1]; // call_cid is formatted as "type:id"
+
+    console.log("meetingId:", meetingId);
+  console.log("transcriptUrl:", event.call_transcription.url);
 
     const [updatedMeeting] = await db
       .update(meetings)
@@ -198,6 +203,7 @@ export async function POST(req: NextRequest) {
       .where(eq(meetings.id, meetingId))
       .returning();
 
+        console.log("updatedMeeting:", updatedMeeting);
     if (!updatedMeeting) {
       return NextResponse.json({ error: "Meeting not found" }, { status: 404 });
     }
